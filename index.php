4a5544a15833e02469be5cd5e1b292f445e94c23
Sunwarul Islam <sanoarulslm@gmail.com>
2024-04-27 19:54:23 +0600

    timzone set Dhaka

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once './core/EnvLoader.php';
 require_once './core/Router.php';
 require_once './core/functions.php';
 
+date_default_timezone_set('Asia/Dhaka');
 class App
 {
     protected DatabaseConnection $dbConnection;
