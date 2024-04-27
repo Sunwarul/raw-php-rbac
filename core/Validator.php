f0833fafd86f29dc001c9b446ae091e276c17b11
Sunwarul Islam <sanoarulslm@gmail.com>
2024-04-28 00:03:03 +0600

    validator enhancement with different types of filters

--- core/Validator.php
+++ core/Validator.php
@@ -2,14 +2,39 @@
 
 class Validator
 {
-    public static function validate($data) : bool
+    public static function validate($data): bool
     {
         $errors = [];
         foreach ($data as $value => $type) {
-            if ($type == 'email') {
-                if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                    $errors[$value] = "Email is not valid";
-                }
+            switch ($type) {
+                case 'numeric':
+                    if (!is_numeric($value)) {
+                        $errors[$value] = $value . ' should be numeric.';
+                    }
+                    break;
+                case 'integer':
+                    if (!is_int($value)) {
+                        $errors[$value] = $value . ' should be integer.';
+                    }
+                    break;
+                case 'float':
+                    if (!is_float($value)) {
+                        $errors[$value] = $value . ' should be float.';
+                    }
+                    break;
+                case 'string':
+                    if (!is_string($value)) {
+                        $errors[$value] = $value . ' should be string.';
+                    }
+                    break;
+                case 'email':
+                    if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                        var_dump($type, $value);
+                        $errors[$value] = $value . ' is invalid email address';
+                    }
+                    break;
+                default:
+                    break;
             }
         }
         $_SESSION['errors'] = $errors;
