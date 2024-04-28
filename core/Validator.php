c3f9f1e34d5095f4d174fb4e22263dd34c5514ef
Sunwarul Islam <sanoarulslm@gmail.com>
2024-04-28 18:23:45 +0600

    validator enhancement with multiple fields

--- core/Validator.php
+++ core/Validator.php
@@ -2,39 +2,46 @@
 
 class Validator
 {
-    public static function validate($data): bool
+    public static function validate($data, $rules): bool
     {
         $errors = [];
-        foreach ($data as $value => $type) {
-            switch ($type) {
-                case 'numeric':
-                    if (!is_numeric($value)) {
-                        $errors[$value] = $value . ' should be numeric.';
-                    }
-                    break;
-                case 'integer':
-                    if (!is_int($value)) {
-                        $errors[$value] = $value . ' should be integer.';
-                    }
-                    break;
-                case 'float':
-                    if (!is_float($value)) {
-                        $errors[$value] = $value . ' should be float.';
-                    }
-                    break;
-                case 'string':
-                    if (!is_string($value) && !is_numeric($value)) {
-                        $errors[$value] = $value . ' should be string.';
-                    }
-                    break;
-                case 'email':
-                    if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                        var_dump($type, $value);
-                        $errors[$value] = $value . ' is invalid email address';
-                    }
-                    break;
-                default:
-                    break;
+        foreach ($rules as $field => $types) {
+            $typesArr = explode('|', $types);
+            foreach ($typesArr as $type) {
+                switch ($type) {
+                    case 'required':
+                        if (isset($data[$field]) && empty($data[$field])) {
+                            $errors[$field] = $field . ' is required.';
+                        }
+                        break;
+                    case 'numeric':
+                        if (!is_numeric($data[$field])) {
+                            $errors[$field] = $field . ' should be numeric.';
+                        }
+                        break;
+                    case 'integer':
+                        if (!is_int($data[$field])) {
+                            $errors[$field] = $field . ' should be integer.';
+                        }
+                        break;
+                    case 'float':
+                        if (!is_float($data[$field])) {
+                            $errors[$field] = $field . ' should be float.';
+                        }
+                        break;
+                    case 'string':
+                        if (!is_string($data[$field]) && !is_numeric($data[$field])) {
+                            $errors[$field] = $field . ' should be string.';
+                        }
+                        break;
+                    case 'email':
+                        if (!filter_var($data[$field], FILTER_VALIDATE_EMAIL)) {
+                            $errors[$field] = $field . ' is invalid email address';
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         $_SESSION['errors'] = $errors;
