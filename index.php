b02c659fc5606bea675f0878a91485dd06d85e28
Sunwarul Islam <sanoarulslm@gmail.com>
2024-04-28 18:58:51 +0600

    Error reporting added

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 require_once './core/EnvLoader.php';
 require_once './core/Router.php';
 require_once './core/functions.php';
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 date_default_timezone_set('Asia/Dhaka');
 class App
 {
     protected DatabaseConnection $dbConnection;
-    
+
     public function __construct()
     {
         EnvLoader::load();
